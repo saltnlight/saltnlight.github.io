6c5eb4d5037b99c23febe04ac206243e892f4603
saltnlight <arongflorence@yahoo.com>
2020-06-19 15:41:48 +0100

    adding javascript and BS-scrollspy

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-let top = document.getElementById('top');
+const bg = document.querySelector('body');
+const form1 = document.querySelector('#popupForm');
+const knowMore = document.querySelector('#pop');
+const closeBar = document.querySelector('#popupCloseBar') 
+
+function togglePopup(){
+    const tog = document.querySelector('#popup');
+    tog.classList.toggle('d-block');
+    // bg.style.backgroundColor();
+}
+
+knowMore.addEventListener('click', togglePopup);
+
+form1.addEventListener('submit', (event) => {
+    event.preventDefault();
+    togglePopup();
+});
+
+closeBar.addEventListener('click', togglePopup);
